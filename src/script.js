f7dce39c4e63531b3dd4eabbe31b5387241758b4
CarlJohn120 <cj.pads120@gmail.com>
2024-11-18 23:16:01 +0800

    13th

--- src/script.js
+++ src/script.js
@@ -32,8 +32,8 @@ window.addEventListener('resize', () =>
     camera.updateProjectionMatrix()
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     })
+
 
 /**
  * Camera
